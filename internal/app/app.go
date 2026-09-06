595001157e9fbd2790b82a147b18c8c68048802f
Vladislav <whatislov@mail.ru>
2026-09-06 22:30:44 +0300

    add postgres

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,11 +3,23 @@ package app
 import (
 	"fmt"
 
+	"context"
+
 	"github.com/Wh4tisl0ve/cloud-file-storage-go/internal/config"
+	"github.com/Wh4tisl0ve/cloud-file-storage-go/pkg/postgres"
 )
 
 func Run(cfg *config.Config) {
-	fmt.Println(cfg)
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		cfg.DBConfig.Username,
+		cfg.DBConfig.Password,
+		cfg.DBConfig.Host,
+		cfg.DBConfig.Port,
+		cfg.DBConfig.Name,
+	)
+	postgres := postgres.New(context.Background(), dsn)
 
+	fmt.Print(postgres)
 	// todo init DB, Cache, S3, logger
 }
---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,34 @@
+package postgres
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Postgres struct {
+	pool *pgxpool.Pool
+}
+
+func New(ctx context.Context, dsn string) *Postgres {
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatalf("ошибка создания пула подключений: %s", err)
+		os.Exit(1)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatalf("ошибка подключения к БД: %s", err)
+		os.Exit(1)
+	}
+
+	return &Postgres{
+		pool: pool,
+	}
+}
+
+func (p *Postgres) Close() {
+	p.pool.Close()
+}
